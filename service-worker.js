aad2e8e96de006339f111aad4dd9f8fb1da361db
Mohd Amjad Ali <2003amjadali@gmail.com>
2025-04-23 18:16:15 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'accident-care-v2';
+const OFFLINE_URL = 'offline.html';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/hospital.html',
+  '/severity.html',
+  '/performance.html',
+  '/styles.css',
+  '/styles2.css',
+  '/styles3.css',
+  '/script.js',
+  '/script2.js',
+  '/script3.js',
+  '/accident%20care.webp',
+  // Add other assets you want to cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => {
+          return caches.match(OFFLINE_URL);
+        })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          return response || fetch(event.request);
+        })
+    );
+  }
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
